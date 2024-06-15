1307ce77245068ccfc5afb24b088857b110859dd
GGN_2015 <premierbob@qq.com>
2024-06-15 10:08:54 +0800

    feat:movie

--- src/Answers.py
+++ src/Answers.py
@@ -7,20 +7,21 @@ from TxtParser import TxtParser
 # 获取所有可能成为问题答案的项目
 class Answers:
     def __init__(self, dir_utils: DirUtils, txt_parser:TxtParser):
-        self.dir_utils      = dir_utils
-        self.txt_parser     = txt_parser
-        self.cached_anwsers = [] # 缓存所有可能答案
-    # 获取 data 文件夹下的所有动漫音乐
-    def get_all_anime_song(self) -> list:
-        if self.cached_anwsers == []:
-            allanime = self.dir_utils.scan_data_dir_with_filename("anime.txt")
-            song_list  = []
-            for filepath in allanime:
-                dic, _ = self.txt_parser.parsefile(filepath) # 解析番剧文件
-                assert dic.get("name") is not None           # 番剧必须指明名字
-                assert dic.get("quarter") is not None        # 番剧必须指明季度
-                anime_name = dic["name"]                     # 获取番剧名称
-                dirnow     = os.path.dirname(filepath)       # mp3 文件必须存储在 txt 文件同目录下
+        self.dir_utils   = dir_utils
+        self.txt_parser  = txt_parser
+        self.last_answer = None
+    # 在指定的可行列表中筛选符合年份要求的歌曲
+    def select_song_list_by_year(self, allanime, year_from, year_to) -> list:
+        song_list  = []
+        for filepath in allanime:
+            dic, _ = self.txt_parser.parsefile(filepath) # 解析文件
+            assert dic.get("name") is not None           # 必须指明名字
+            assert dic.get("quarter") is not None        # 必须指明季度
+            anime_name = dic["name"]                     # 获取名称
+            quarter    = dic.get("quarter")              # 获取年月
+            year       = int(quarter.split("-")[0])      # 获取年份
+            dirnow     = os.path.dirname(filepath)       # mp3 文件必须存储在 txt 文件同目录下
+            if year_from <= year <= year_to:             # 如果在指定年份区间里
                 for term in dic:     
                     song_name = dic[term]
                     if term.endswith(".mp3"):
@@ -32,16 +33,48 @@ class Answers:
                             "tag" : tag, # 歌曲的显示名称
                             "file": final_file # 歌曲比较的主键：文件路径
                         })
-            self.cached_anwsers = song_list
-        return self.cached_anwsers
+        return song_list
+    # 获取 data 文件夹下的所有动漫音乐
+    def get_all_anime_song(self, year_from, year_to) -> list:
+        allanime = self.dir_utils.scan_data_dir_with_filename("anime.txt")
+        return self.select_song_list_by_year(allanime, year_from, year_to)
+    # 获得所有电影中的音乐
+    def get_all_movie_song(self, year_from, year_to):
+        allanime = self.dir_utils.scan_data_dir_with_filename("movie.txt")
+        return self.select_song_list_by_year(allanime, year_from, year_to)
+    def get_all_jpnv_song(self, year_from, year_to):
+        return []
+    def get_all_chnv_song(self, year_from, year_to):
+        return []
+    # 获取所有歌曲
+    def get_all_song(self, year_from, year_to, anime: bool, movie: bool, jpnv: bool, chnv: bool):
+        arr = []
+        if anime:
+            arr += self.get_all_anime_song(year_from, year_to)
+        if movie:
+            arr += self.get_all_movie_song(year_from, year_to)
+        if jpnv:
+            arr += self.get_all_jpnv_song(year_from, year_to)
+        if chnv:
+            arr += self.get_all_chnv_song(year_from, year_to)
+        return arr
     # 生成一个随机问题
-    def generate_random_question(self, answer_cnt: int):
-        answer_cnt = min(answer_cnt, len(self.get_all_anime_song()))
-        return random.sample(self.get_all_anime_song(), answer_cnt)
+    def generate_random_question(self, options_cnt: int, year_from, year_to, anime: bool, movie: bool, jpnv: bool, chnv: bool):
+        assert options_cnt >= 2 # 至少要有两个选项
+        all_song = self.get_all_song(year_from, year_to, anime, movie, jpnv, chnv)
+        if options_cnt > len(all_song):
+            return [] # 返回一个空问题，表示不存在合法的问题
+        else:
+            ans_list = random.sample(all_song, options_cnt)
+            retry_cnt = 0
+            while ans_list[0] == self.last_answer and retry_cnt < 3: # 尽可能保证相邻两次查询答案不同
+                retry_cnt += 1
+                ans_list = random.sample(all_song, options_cnt)
+            return ans_list
 
 if __name__ == "__main__":
     dir_utils  = DirUtils()
     txt_parser = TxtParser()
     answers    = Answers(dir_utils, txt_parser)
-    for pr in answers.generate_random_question(4):
+    for pr in answers.generate_random_question(4, 1908, 2100, True, True, True, True):
         print(pr)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 import random
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request
 
 from DirUtils  import DirUtils
 from TxtParser import TxtParser
@@ -11,12 +11,44 @@ txt_parser = TxtParser()
 answers    = Answers(dir_utils, txt_parser)
 app        = Flask(__name__)
 
+# 如果是合法整数，则返回，否则返回默认值
+def wrap_int(x: str, default_v:int):
+    try:
+        x = int(x)
+    except:
+        x = default_v
+    return x
+
 # 首页
 @app.route('/')
 def index():
-    options = answers.generate_random_question(4)
-    answer  = random.sample(options, 1)[0]
-    return render_template('index.html', options=options, answer=answer)
+    year_from      = wrap_int(request.args.get("year_from", ""), 1908)
+    year_to        = wrap_int(request.args.get("year_to", ""), 2100)
+    year_to        = max(year_to, year_from)
+    anime_checkbox = request.args.get("anime_checkbox", "0")
+    movie_checkbox = request.args.get("movie_checkbox", "0")
+    jpn_vocaloid   = request.args.get("jpn_vocaloid_checkbox", "0")
+    chn_vocaloid   = request.args.get("chn_vocaloid_checkbox", "0")
+    if set([anime_checkbox, movie_checkbox, jpn_vocaloid, chn_vocaloid]) == set(["0"]):
+        anime_checkbox = "1"
+        movie_checkbox = "1"
+        jpn_vocaloid   = "1"
+        chn_vocaloid   = "1" # 默认全选
+    options = answers.generate_random_question(4, year_from, year_to, anime_checkbox=="1", movie_checkbox=="1", jpn_vocaloid=="1", chn_vocaloid=="1")
+    if options != []:
+        answer = options[0] # 默认第一个选项是答案
+    else:
+        answer = None       # 问题不存在
+    random.shuffle(options) # 重新打乱
+    return render_template('index.html', 
+        options=options, 
+        answer=answer,
+        year_from=year_from,
+        year_to=year_to,
+        anime_checkbox=anime_checkbox,
+        movie_checkbox=movie_checkbox,
+        jpn_vocaloid_checkbox=jpn_vocaloid,
+        chn_vocaloid_checkbox=chn_vocaloid)
 
 # 提供音频文件的接口
 @app.route('/audio/<path:file_path>')
